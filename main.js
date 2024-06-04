918345a8fd321713b66ad755061ba7cd9b9b3102
ZennKurra <135609265+ZennKurra@users.noreply.github.com>
2024-06-04 20:20:55 +0800

    Import my calculator.gltf to the scene

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import './style.css';
 
 const scene = new THREE.Scene();
+const gltfLoader = new GLTFLoader();
 
 const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.setZ( 30 );
+camera.position.setZ( 5 );
 
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canvas"), alpha: true });
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 
-const geometry = new THREE.BoxGeometry( 15, 15, 15 );
-const material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true });
-const cube = new THREE.Mesh( geometry, material );
+gltfLoader.load("./assets/models/calculator.gltf", (gltf) => {
+  const model = gltf.scene;
+  scene.add( model );
+});
 
-scene.add( cube );
 renderer.render( scene, camera );
 
 function animate() {
   requestAnimationFrame( animate );
 
-  cube.rotation.x += 0.005;
-  cube.rotation.y += 0.01;
-  cube.rotation.z += 0.005;
-  
   renderer.render( scene, camera );
 }
 
