68a226863e964bbeca4f74330406dacb84a0c6d6
ZennKurra <135609265+ZennKurra@users.noreply.github.com>
2024-06-04 18:47:27 +0800

    Setup initial Canvas, Scene, Camera, and Renderer

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-import './style.css'
+import * as THREE from 'three';
+import './style.css';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.setZ( 30 );
+
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canvas"), alpha: true });
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.render( scene, camera );
+
+window.addEventListener("resize", () => {
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
