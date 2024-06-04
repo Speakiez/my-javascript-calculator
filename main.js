a044f991811141115e2eb04f36976589ae46d321
ZennKurra <135609265+ZennKurra@users.noreply.github.com>
2024-06-04 19:04:19 +0800

    Add a simple example cube onto the canvas

--- main.js
+++ main.js
@@ -9,10 +9,29 @@ camera.position.setZ( 30 );
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canvas"), alpha: true });
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
+
+const geometry = new THREE.BoxGeometry( 15, 15, 15 );
+const material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true });
+const cube = new THREE.Mesh( geometry, material );
+
+scene.add( cube );
 renderer.render( scene, camera );
 
+function animate() {
+  requestAnimationFrame( animate );
+
+  cube.rotation.x += 0.005;
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.005;
+  
+  renderer.render( scene, camera );
+}
+
+animate();
+
 window.addEventListener("resize", () => {
-  renderer.setSize( window.innerWidth, window.innerHeight );
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
 });
